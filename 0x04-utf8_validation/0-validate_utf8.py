b7cc875d83f4b7821b3db2fcba62fd4364a0d4a1
Emedo586 <emedopeace99@gmail.com>
2024-05-12 09:48:56 +0000

    Amendments

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
+"""
+Define UTF-8 Validation function:
+method that determines if a given data set
+represents a valid UTF-8 encoding
+"""
+
+
 def validUTF8(data):
-    """method that determines if a given data set
-    represents a valid UTF-8 encoding
     """
-    nbytes = 0
-    b1 = 1 << 7
-    b2 = 1 << 6
-    for i in data:
-        b = 1 << 7
-        if nbytes == 0:
-            while b & i:
-                nbytes += 1
-                b = b >> 1
-                if nbytes == 0:
-                    continue
-                if nbytes == 1 or nbytes > 4:
-                    return False
-        else:
-            if not (i & b1 and not (i & b2)):
+    UTF-8 Validation
+    """
+    expected_bytes = 0
+    s1 = 1 << 7
+    s2 = 1 << 6
+
+    for num in data:
+        binary_str = 1 << 7
+        if expected_bytes == 0:
+            while binary_str & num:
+                expected_bytes += 1
+                binary_str = binary_str >> 1
+            if expected_bytes == 0:
+                continue
+            if expected_bytes == 1 or expected_bytes > 4:
                 return False
-        nbytes -= 1
-    return nbytes == 0
+        else:
+            if not (num & s1 and not (num & s2)):
+                return False
+        expected_bytes -= 1
+    #return not expected_bytes
+    return expected_bytes == 0
