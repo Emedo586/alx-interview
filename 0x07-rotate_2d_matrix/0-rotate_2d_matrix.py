19dc91fd3f8c7be8ed989c796a1013ec17cf6245
Emedo586 <emedopeace99@gmail.com>
2024-05-28 02:34:10 +0000

    2D MATRIX

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a 2D matrix 90 degrees clockwise.
+    """
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        # Outer loop iterates through the row of matrix upto half its length
+        for j in range(i, (n - i - 1)):
+            # Inner loop iterates through the column within the current row
+            x = (n - 1 - j)
+            # Retrive elements in current group and swap them.
+            p =  matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[(n - i - 1)][x]
+            matrix[(n - i - 1)][x] = matrix[j][(n - i - 1)]
+            matrix[j][(n - i - 1)] = p
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,3 @@
+# Rotate-2D Matrix
+
+![](https://i.pinimg.com/564x/96/0d/35/960d35f0343b9abe77cb2c5a14d3de6a.jpg)
