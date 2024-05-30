6ad84730575a84bb3edccb83d3ebc77ebd2666b5
Emedo586 <emedopeace99@gmail.com>
2024-05-30 13:55:58 +0000

    amazing days

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -15,7 +15,7 @@ def rotate_2d_matrix(matrix):
             # Inner loop iterates through the column within the current row
             x = (n - 1 - j)
             # Retrive elements in current group and swap them.
-            p =  matrix[i][j]
+            p = matrix[i][j]
             matrix[i][j] = matrix[x][i]
             matrix[x][i] = matrix[(n - i - 1)][x]
             matrix[(n - i - 1)][x] = matrix[j][(n - i - 1)]
